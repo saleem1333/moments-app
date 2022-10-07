31165ffe4f20a11321ac6a5a61fb53445950348d
saleem1333 <saleem_8@hotmail.com>
2022-10-07 19:13:00 +0200

    Added BlocListener for SignInFormScreen and SignUpFormScreen

--- lib/presentation/screens/auth/sign_up_form/sign_up_form_screen.dart
+++ lib/presentation/screens/auth/sign_up_form/sign_up_form_screen.dart
@@ -6,6 +6,7 @@ import 'package:moments_app/presentation/core/widgets/circular_svg_button.dart';
 import 'package:moments_app/presentation/core/widgets/main_textfield.dart';
 
 import '../../../../application/auth/sign_up_form/sign_up_form_cubit.dart';
+import '../../../../application/auth/sign_up_form/sign_up_form_state.dart';
 import '../../../../injections.dart';
 import '../../../core/config/app_colors.dart';
 import '../../../core/config/app_text_styles.dart';
@@ -36,139 +37,148 @@ class _Body extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     final state = context.watch<SignUpFormCubit>().state;
 
-    return Form(
-      autovalidateMode: state.showErrors
-          ? AutovalidateMode.always
-          : AutovalidateMode.disabled,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(size.width * .05),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: size.height * .3,
-                child: Padding(
-                  padding: EdgeInsets.all(size.width * .025),
-                  child: SvgPicture.asset(
-                    SvgPaths.signup,
+    return BlocListener<SignUpFormCubit, SignUpFormState>(
+      listener: (context, state) {
+        state.authFailureOrSuccess?.fold((f) {
+          // handle failure logic
+        }, (_) {
+          // handle success logic
+        });
+      },
+      child: Form(
+        autovalidateMode: state.showErrors
+            ? AutovalidateMode.always
+            : AutovalidateMode.disabled,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(size.width * .05),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: size.height * .3,
+                  child: Padding(
+                    padding: EdgeInsets.all(size.width * .025),
+                    child: SvgPicture.asset(
+                      SvgPaths.signup,
+                    ),
                   ),
                 ),
-              ),
-              Text(
-                "Sign Up",
-                style: AppTextStyles.styleWeight900(
-                  color: Colors.black,
-                  fontSize: size.width * .075,
+                Text(
+                  "Sign Up",
+                  style: AppTextStyles.styleWeight900(
+                    color: Colors.black,
+                    fontSize: size.width * .075,
+                  ),
                 ),
-              ),
-              SizedBox(height: size.width * .03),
-              Text(
-                "Username",
-                style: AppTextStyles.styleWeight700(
-                  fontSize: size.width * .04,
+                SizedBox(height: size.width * .03),
+                Text(
+                  "Username",
+                  style: AppTextStyles.styleWeight700(
+                    fontSize: size.width * .04,
+                  ),
                 ),
-              ),
-              MainTextfield(
-                hint: "Enter Username",
-                onChanged: (value) =>
-                    context.read<SignUpFormCubit>().usernameChanged(value),
-                validator: (_) => context
-                    .read<SignUpFormCubit>()
-                    .state
-                    .username
-                    .failureOrValue
-                    .fold((f) => f.message, (_) => null),
-              ),
-              SizedBox(height: size.width * .03),
-              Text(
-                "Email",
-                style: AppTextStyles.styleWeight700(
-                  fontSize: size.width * .04,
-                ),
-              ),
-              MainTextfield(
-                hint: "Enter Email",
-                onChanged: (value) =>
-                    context.read<SignUpFormCubit>().emailChanged(value),
-                validator: (_) => context
-                    .read<SignUpFormCubit>()
-                    .state
-                    .emailAddress
-                    .failureOrValue
-                    .fold((f) => f.message, (_) => null),
-              ),
-              SizedBox(height: size.width * .04),
-              Text(
-                "Password",
-                style: AppTextStyles.styleWeight700(
-                  fontSize: size.width * .04,
-                ),
-              ),
-              MainTextfield(
-                hint: "Enter Password",
-                isPassword: true,
-                onChanged: (value) =>
-                    context.read<SignUpFormCubit>().passwordChanged(value),
-                validator: (_) => context
-                    .read<SignUpFormCubit>()
-                    .state
-                    .password
-                    .failureOrValue
-                    .fold((f) => f.message, (_) => null),
-              ),
-              SizedBox(height: size.width * .04),
-              Text(
-                "Confirm Password",
-                style: AppTextStyles.styleWeight700(
-                  fontSize: size.width * .04,
-                ),
-              ),
-              MainTextfield(
-                  hint: "Enter Confirm Password",
-                  isPassword: true,
-                  onChanged: (value) => context
-                      .read<SignUpFormCubit>()
-                      .confirmationPasswordChanged(value),
+                MainTextfield(
+                  hint: "Enter Username",
+                  onChanged: (value) =>
+                      context.read<SignUpFormCubit>().usernameChanged(value),
                   validator: (_) => context
                       .read<SignUpFormCubit>()
                       .state
-                      .confirmationPassword
+                      .username
                       .failureOrValue
-                      .fold((f) => f.message, (_) => null)),
-              SizedBox(height: size.width * .09),
-              MainButton(
-                text: "Sign up",
-                color: AppColors.mainColor,
-                width: size.width,
-                onPressed: () async {
-                  await context.read<SignUpFormCubit>().submit();
-                },
-              ),
-              SizedBox(height: size.width * .06),
-              TextDivider(text: "Or continue with"),
-              SizedBox(height: size.width * .04),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircularSvgButton(
-                    size: size,
-                    svgPath: SvgPaths.google,
-                    onPressed: () {},
+                      .fold((f) => f.message, (_) => null),
+                ),
+                SizedBox(height: size.width * .03),
+                Text(
+                  "Email",
+                  style: AppTextStyles.styleWeight700(
+                    fontSize: size.width * .04,
                   ),
-                  CircularSvgButton(
-                    size: size,
-                    svgPath: SvgPaths.facebook,
-                    onPressed: () {},
+                ),
+                MainTextfield(
+                  hint: "Enter Email",
+                  onChanged: (value) =>
+                      context.read<SignUpFormCubit>().emailChanged(value),
+                  validator: (_) => context
+                      .read<SignUpFormCubit>()
+                      .state
+                      .emailAddress
+                      .failureOrValue
+                      .fold((f) => f.message, (_) => null),
+                ),
+                SizedBox(height: size.width * .04),
+                Text(
+                  "Password",
+                  style: AppTextStyles.styleWeight700(
+                    fontSize: size.width * .04,
                   ),
-                  CircularSvgButton(
-                    size: size,
-                    svgPath: SvgPaths.apple,
-                    onPressed: () {},
+                ),
+                MainTextfield(
+                  hint: "Enter Password",
+                  isPassword: true,
+                  onChanged: (value) =>
+                      context.read<SignUpFormCubit>().passwordChanged(value),
+                  validator: (_) => context
+                      .read<SignUpFormCubit>()
+                      .state
+                      .password
+                      .failureOrValue
+                      .fold((f) => f.message, (_) => null),
+                ),
+                SizedBox(height: size.width * .04),
+                Text(
+                  "Confirm Password",
+                  style: AppTextStyles.styleWeight700(
+                    fontSize: size.width * .04,
                   ),
-                ],
-              ),
-            ],
+                ),
+                MainTextfield(
+                    hint: "Enter Confirm Password",
+                    isPassword: true,
+                    onChanged: (value) => context
+                        .read<SignUpFormCubit>()
+                        .confirmationPasswordChanged(value),
+                    validator: (_) => context
+                        .read<SignUpFormCubit>()
+                        .state
+                        .confirmationPassword
+                        .failureOrValue
+                        .fold((f) => f.message, (_) => null)),
+                SizedBox(height: size.width * .09),
+                MainButton(
+                  text: "Sign up",
+                  color: AppColors.mainColor,
+                  width: size.width,
+                  onPressed: () async {
+                    await context.read<SignUpFormCubit>().submit();
+                  },
+                ),
+                SizedBox(height: size.width * .06),
+                TextDivider(text: "Or continue with"),
+                SizedBox(height: size.width * .04),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircularSvgButton(
+                      size: size,
+                      svgPath: SvgPaths.google,
+                      onPressed: () {},
+                    ),
+                    CircularSvgButton(
+                      size: size,
+                      svgPath: SvgPaths.facebook,
+                      onPressed: () {},
+                    ),
+                    CircularSvgButton(
+                      size: size,
+                      svgPath: SvgPaths.apple,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
