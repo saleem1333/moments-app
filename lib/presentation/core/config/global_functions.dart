2f13093663f1f854094c123d0c14c3c4c6633b70
os01ri <osamahm2001@gmail.com>
2022-10-21 20:31:49 +0300

    adding show snackbar function

--- lib/presentation/core/config/global_functions.dart
+++ lib/presentation/core/config/global_functions.dart
@@ -6,9 +6,66 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:moments_app/presentation/core/config/app_colors.dart';
+import 'package:moments_app/presentation/core/config/app_text_styles.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class GlobalUiFunctions {
+  showMainSnackbar({
+    required BuildContext context,
+    required String title,
+    required String description,
+    Color snackColor = AppColors.grey2,
+    Widget? leading,
+    Widget? action,
+  }) {
+    final size = MediaQuery.of(context).size;
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Container(
+          height: size.height * .1,
+          padding: EdgeInsets.all(size.width * .03),
+          decoration: BoxDecoration(
+            color: snackColor,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              leading ?? const SizedBox(),
+              SizedBox(width: size.width * .03),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    title,
+                    style: AppTextStyles.styleWeight600(
+                      color: Colors.black,
+                      fontSize: size.width * .035,
+                    ),
+                  ),
+                  Text(
+                    description,
+                    maxLines: 2,
+                    style: AppTextStyles.styleWeight300(
+                      color: Colors.black,
+                      fontSize: size.width * .033,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              action ?? const SizedBox(),
+            ],
+          ),
+        ),
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+    );
+  }
+
   static Widget loading({
     double size = 35,
     Color color = AppColors.mainColor,
